672cb1964de57859941864ef6a5a15ca20aa1215
GutekDeveloper <medopiekun.gutek@gmail.com>
2025-11-03 15:30:46 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const tekstContainer = document.getElementById("tekst-leki");
-    if (tekstContainer && typeof tekstLeki !== "undefined") {
-        tekstContainer.innerHTML = tekstLeki;
-    }
-});
