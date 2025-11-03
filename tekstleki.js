4e03f1285e75bb6ea886dad8d514a9527bc6de4c
GutekDeveloper <medopiekun.gutek@gmail.com>
2025-11-03 01:09:55 +0100

    Create tekstleki.js

---
+++ tekstleki.js
@@ -0,0 +1,5 @@
+const tekstLeki = `
+<h3>Sekcja: Leki</h3>
+<p>Aplikacja MedOpiekun pomaga w zarządzaniu Twoimi lekami — przypomina o porach dawkowania, dawkach oraz terminach recept.</p>
+<p>W prosty sposób możesz dodać, edytować lub usunąć lek, a także śledzić historię stosowania.</p>
+`;
