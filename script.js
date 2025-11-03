7248a163bfc7f7e5d99f866a5d18fd906c3d08d8
GutekDeveloper <medopiekun.gutek@gmail.com>
2025-11-03 01:11:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Witamy w aplikacji MedOpiekun!");
+// Wstawienie tekstu sekcji Leki
+document.addEventListener("DOMContentLoaded", () => {
+    const tekstContainer = document.getElementById("tekst-leki");
+    if (tekstContainer && typeof tekstLeki !== "undefined") {
+        tekstContainer.innerHTML = tekstLeki;
+    }
+});
