be44a42290f63ef9f9a334fd2e0cba6fa4a06e8c
GutekDeveloper <medopiekun.gutek@gmail.com>
2025-11-07 17:12:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,21 @@ function revealSectionsOnScroll() {
 
 window.addEventListener("scroll", revealSectionsOnScroll);
 window.addEventListener("DOMContentLoaded", revealSectionsOnScroll);
+
+// Skrypt do ukrywania/pokazywania nagłówka przy przewijaniu
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (currentScroll > lastScrollTop && currentScroll > 100) {
+    // Jeśli przewijamy w dół, ukryj nagłówek
+    header.style.top = '-100px'; 
+  } else {
+    // Jeśli przewijamy w górę, pokaż nagłówek
+    header.style.top = '0'; 
+  }
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
+
